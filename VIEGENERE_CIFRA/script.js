ca21434367c9c4c015706e5706fb7a94c25cd69b
vinixavi95 <89113667+vinixavi95@users.noreply.github.com>
2022-09-05 11:19:16 -0300

    Update script.js

--- VIEGENERE_CIFRA/script.js
+++ VIEGENERE_CIFRA/script.js
@@ -1,35 +1,36 @@
+//************ CIFRA DE VIGENERE ************//
 //LISTA COM O ALFABETO ORIGINAL UTILIZADO PARA CIFRAGEM
 const alfabeto_original = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //ENTRADA DE DADOS, ONDE BUSCAREMOS O INPUT DO USUÁRIO NO DOCUMENTO HTML
-let texto = "resignacao";
-let chave = "deus";
+let texto = 'vamos codar';
+let chave = 'react';
+let opcao = '1';
 
 let textoFinal = []; //VARIAVEL LISTA QUE RECEBERÁ O TEXTO FINAL CRIPTOGRAFADO
 
 for (let i = 0; i < texto.length; i++) { //LAÇO DESLOCA A POSICAO DO ELEMENTO ATRAVÉS DO INDICE PARA GERAR 26 ALFABETOS CIFRADO
 	
-  let tamanhoChave = chave.length; //VARIAVEL QUE ARMAZENA O TAMANHO DA PALAVRA-CHAVE
+  	let tamanhoChave = chave.length; //VARIAVEL QUE ARMAZENA O TAMANHO DA PALAVRA-CHAVE
 	let caracter = texto[i]; //VARIAVEL ARMAZENA CADA LETRA DO TEXTO A SER CIFRADO
 	let indice = alfabeto_original.indexOf(caracter); //VARIAVEL QUE ARMAZENA OS INDICES DE CADA LETRA DO TEXTO A SER CIFRADO
+	
 	let indice_chave;//VARIAVEL QUE ARMAZENA O INDICE DA PALAVRA-CHAVE
+	indice_chave = alfabeto_original.indexOf(chave[i % tamanhoChave]);
+	
 
+	let encriptar = indice + indice_chave; //VARIAVEL QUE SOMA  (INDICE DO TEXTO + INDICE DA PALAVRA-CHAVE)
+    let descriptar = indice - indice_chave + 26; //VARIAVEL QUE SUBTRAI  (INDICE DO TEXTO - INDICE DA PALAVRA-CHAVE + 26)
 
-	if (i > tamanhoChave) {
-		indice_chave = alfabeto_original.indexOf(chave[i % tamanhoChave]); //VARIAVEL QUE ARMAZENA O INDICE DE CADA LETRA DA CHAVE
-	} 
-
-  else {
-		indice_chave = alfabeto_original.indexOf(chave[i % tamanhoChave]);
+	if (opcao == '1') {
+		encriptar = encriptar % 26; //VARIAVEL QUE FINALIZA O CALCULO CRIPTOGRAFICO (INDICE_TEXTO + INDICE_CHAVE % 26) (ENCRIPTAR)
 	}
 
-	let indice_final = indice + indice_chave; //VARIAVEL QUE SOMA O INDICE DO TEXTO + INDICE DA PALAVRA-CHAVE
+    else if (opcao == '2' ) {
+        descriptar = descriptar % 26; //VARIAVEL QUE FINALIZA O CALCULO CRIPTOGRAFICO (INDICE_TEXTO - INDICE_CHAVE + 26 % 26 (DESCRIPTAR)
+    }
 
-	if (indice_final >= 26) {
-		indice_final = indice_final % 26; //VARIAVEL QUE FINALIZA O CALCULO CRIPTOGRAFICO (INDICE_TEXTO + INDICE_CHAVE % 26)
-	}
-
-	textoFinal.push(alfabeto_original[indice_final]);
+	textoFinal.push(alfabeto_original[encriptar]);
 }
 
 let texto_final = textoFinal.join(""); //VARIAVEL QUE RECEBE O TEXTO ENCRIPTADO
